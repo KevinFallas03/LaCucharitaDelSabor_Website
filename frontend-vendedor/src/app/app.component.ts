abf17e7e1917db1ac6019f1c4605372b0833da1e
Kevin Fallas <kf.ma@estudiantec.cr>
2021-05-08 23:55:24 -0600

    Titles updated

--- frontend-vendedor/src/app/app.component.ts
+++ frontend-vendedor/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend-vendedor';
+  title = 'Administración de La Cucharita Del Sabor';
 }

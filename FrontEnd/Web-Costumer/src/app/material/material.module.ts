705f522371d3739b90e1d080eb82860526e20169
Ignacio Álvarez <nacho.alvarez2001@gmail.com>
2021-06-04 16:39:04 -0600

    Added Header & Banner For Client Page

--- FrontEnd/Web-Costumer/src/app/material/material.module.ts
+++ FrontEnd/Web-Costumer/src/app/material/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 const MaterialComponents = [ 
   MatButtonModule,
+  MatDividerModule,
+  MatListModule,
 ];
 
 @NgModule({

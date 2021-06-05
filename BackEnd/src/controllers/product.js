fd32bab9c0a0a7262057860ea1db4f7118d76dc6
Kevin Fallas <kf.ma@estudiantec.cr>
2021-06-05 13:27:26 -0600

    Update db connection and Add get product by id

--- BackEnd/src/controllers/product.js
+++ BackEnd/src/controllers/product.js
@@ -11,11 +11,21 @@ productController.getAllProducts = async (req,res) => {
     }
 }
 
+//GET PRODUCT BY ID
+productController.getProduct = async (req,res) => {
+    try{
+        const products = await Product.find({_id: req.params.id});
+        res.json(products);
+    }catch (error){
+        res.json({message: error})
+    }
+}
+
 productController.createProduct = async (req,res) => {
     const product = new Product({
         name: req.body.name,
         price: req.body.price,
-        portions: req.body.prortions,
+        portions: req.body.portions,
         image: req.body.image
     });
     try{

8f9f03fd8cad1e4513dd82dfcffdc5d22d1221ba
Kevin Fallas <kf.ma@estudiantec.cr>
2021-06-05 10:22:51 -0600

    Add basic product crud

--- BackEnd/src/controllers/product.js
+++ BackEnd/src/controllers/product.js
@@ -1,28 +1,48 @@
-const productSchema = require("../models/Product");
+const Product = require("../models/Product");
 const productController = {};
 
-productController.getProduct = async (req,res) => {
-    res.json({
-        message : "Get from product"
-    });
+// GET ALL PRODUCTS 
+productController.getAllProducts = async (req,res) => {
+    try{
+        const products = await Product.find({});
+        res.json(products);
+    }catch (error){
+        res.json({message: error})
+    }
 }
 
 productController.createProduct = async (req,res) => {
-    res.json({
-        message : "Create from product"
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        portions: req.body.prortions,
+        image: req.body.image
     });
+    try{
+        const savedProduct = await product.save();
+        res.json(savedProduct);
+
+    }catch (error){
+        res.json({message: error})
+    }
 }
 
 productController.updateProduct = async (req,res) => {
-    res.json({
-        message : "Update from product"
-    });
+    try{
+        const updatedProduct = await Product.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
+        res.json(updatedProduct);
+    } catch (error) {
+        res.json({message: error});
+    }
 }
 
 productController.deleteProduct = async (req,res) => {
-    res.json({
-        message : "Delete from product"
-    });
+    try {
+        const removedProduct = await Product.findOneAndDelete({_id: req.params.id});
+        res.json(removedProduct);
+    } catch (error) {
+        res.json({message : error});
+    }
 }
 
 module.exports = productController;

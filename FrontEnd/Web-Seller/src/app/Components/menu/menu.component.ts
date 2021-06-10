c2a8e3c6fcee1f34ec4b7ecd8a09df5a826d45ff
Kevin Fallas <kf.ma@estudiantec.cr>
2021-06-10 12:47:26 -0600

    Add call to edit and create dialog
    it has the products hardcoded and without carrousel

--- FrontEnd/Web-Seller/src/app/Components/menu/menu.component.ts
+++ FrontEnd/Web-Seller/src/app/Components/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 @Component({
   selector: 'app-menu',
@@ -11,11 +13,28 @@ export class MenuComponent implements OnInit {
   public products = [
     {name:"Tiramisú",price:14000,portions:16,image:"https://i.imgur.com/MjnbeUg.png"},
     {name:"Prestiño",price:5000,portions:5,image:"https://i.imgur.com/6ybxzES.png"},
+    {name:"Prestiño",price:5000,portions:5,image:"https://i.imgur.com/6ybxzES.png"},
   ];
 
-  constructor() { }
-
+  constructor(private dialog: MatDialog) { }
+  
   ngOnInit(): void {
+  }
+
+  onCreate() {
+    const dialogConfig = new MatDialogConfig();
+    //dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    //dialogConfig.width = "60%";
+    this.dialog.open(CreateProductComponent, dialogConfig);
+  }
+
+  onEdit() {
+    const dialogConfig = new MatDialogConfig();
+    //dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    //dialogConfig.width = "60%";
+    this.dialog.open(EditProductComponent, dialogConfig);
   }
 
 }

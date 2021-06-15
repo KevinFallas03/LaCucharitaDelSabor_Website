b750781898cc1ad3655406b1affcba6653d6d929
Kevin Fallas <kf.ma@estudiantec.cr>
2021-06-15 09:22:07 -0600

    Add User service

---
+++ FrontEnd/Web-Seller/src/app/Models/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+    _id?: string;
+    name?: string;
+    email?: string;
+    jobTitle?: string;
+    isAdmin?: boolean;
+}
+  
+export default User;
---
+++ FrontEnd/Web-Seller/src/app/Services/User/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  _apiUrl = environment.url + "/api/user";
+
+  constructor(private _http: HttpClient) { }
+
+  getAllUsers(){
+    return this._http.get<any>(this._apiUrl);
+  }
+
+  getUser(id: any){
+    return this._http.get<any>(
+      `${this._apiUrl}/${id}`
+    );
+  }
+
+  postUser(user: any){
+    return this._http.post<any>(this._apiUrl, user)
+  }
+
+  findByIdAndUpdate(id: any, updatedUser: any){
+    return this._http.put(this._apiUrl+'/'+id, updatedUser);
+  }
+
+  deleteById(id: any){
+    return this._http.delete(this._apiUrl+'/'+id);
+  }
+}
